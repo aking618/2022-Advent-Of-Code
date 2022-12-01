3919f30841e7286c90ac2041174369476a4f288b
Ayren King <56404603+aking618@users.noreply.github.com>
2022-12-01 21:20:13 +0000

    Day 1 : Complete

---
+++ Day1/index.js
@@ -0,0 +1,37 @@
+let split = (input.split("\n\n"))
+
+let sums = [];
+let max = sum = 0;
+
+for (let i = 0; i < split.length; i++) {
+  let lines = split[i].split("\n")
+  sum = 0;
+
+  for (let j = 0; j < lines.length; j++) {
+    sum += parseInt(lines[j]);
+  }
+
+  sums.push(sum);
+}
+
+let firstMax = Math.max(...sums)
+sums = sums.filter(e => e !== firstMax)
+
+console.log(firstMax)
+
+let secondMax = Math.max(...sums)
+sums = sums.filter(e => e !== secondMax)
+
+let thirdMax = Math.max(...sums)
+sums = sums.filter(e => e !== thirdMax)
+
+console.log(firstMax + secondMax + thirdMax)
+
+let div = document.createElement('div')
+div.innerHTML = `${firstMax + secondMax + thirdMax}`
+document.getElementById("main").appendChild(div)
+
+
+
+
+
