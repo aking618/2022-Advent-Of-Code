4ef23bd7c3b5012b354b4c0c591061b887d63ba9
Ayren King <ashsquads@gmail.com>
2022-12-02 16:40:40 -0600

    Day2 : cleanup logic

--- Day2/index.js
+++ Day2/index.js
@@ -1,93 +1,25 @@
-const checkMatch = (move1, move2) => {
-    let move1A = move1 === "A";
-    let move1B = move1 === "B";
-    let move1C = move1 === "C";
-
-    let move2X = move2 === "X";
-    let move2Y = move2 === "Y";
-    let move2Z = move2 === "Z";
-
-    if (move1A && move2X) {
-        return 1 + 3;
-    }
-
-    if (move1A && move2Y) {
-        return 2 + 6;
-    }
-
-    if (move1A && move2Z) {
-        return 3 + 0;
-    }
-
-    if (move1B && move2X) {
-        return 1 + 0;
-    }
-
-    if (move1B && move2Y) {
-        return 2 + 3;
-    }
-
-    if (move1B && move2Z) {
-        return 3 + 6;
-    }
-
-    if (move1C && move2X) {
-        return 1 + 6;
-    }
-
-    if (move1C && move2Y) {
-        return 2 + 0;
-    }
-
-    if (move1C && move2Z) {
-        return 3 + 3;
-    }
+const matchRubric = {
+    "A X": 4,
+    "A Y": 8,
+    "A Z": 3,
+    "B X": 1,
+    "B Y": 5,
+    "B Z": 9,
+    "C X": 7,
+    "C Y": 2,
+    "C Z": 6,
 };
 
-const checkModifiedMatch = (move1, move2) => {
-    let move1A = move1 === "A";
-    let move1B = move1 === "B";
-    let move1C = move1 === "C";
-
-    let lose = move2 === "X";
-    let draw = move2 === "Y";
-    let win = move2 === "Z";
-
-    if (move1A && lose) {
-        return 3 + 0;
-    }
-
-    if (move1A && draw) {
-        return 1 + 3;
-    }
-
-    if (move1A && win) {
-        return 2 + 6;
-    }
-
-    if (move1B && lose) {
-        return 1 + 0;
-    }
-
-    if (move1B && draw) {
-        return 2 + 3;
-    }
-
-    if (move1B && win) {
-        return 3 + 6;
-    }
-
-    if (move1C && lose) {
-        return 2 + 0;
-    }
-
-    if (move1C && draw) {
-        return 3 + 3;
-    }
-
-    if (move1C && win) {
-        return 1 + 6;
-    }
+const modifiedMatchRubric = {
+    "A X": 3,
+    "A Y": 4,
+    "A Z": 8,
+    "B X": 1,
+    "B Y": 5,
+    "B Z": 9,
+    "C X": 2,
+    "C Y": 6,
+    "C Z": 7,
 };
 
 let games = input.split("\n");
@@ -96,10 +28,8 @@ let partOneTotal = 0;
 let partTwoTotal = 0;
 
 for (let i = 0; i < games.length; i++) {
-    let options = games[i].split(" ");
-
-    partOneTotal += checkMatch(options[0], options[1]);
-    partTwoTotal += checkModifiedMatch(options[0], options[1]);
+    partOneTotal += matchRubric[games[i]];
+    partTwoTotal += modifiedMatchRubric[games[i]];
 }
 
 console.log(partOneTotal);
